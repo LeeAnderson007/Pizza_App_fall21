6ac0358876547aae8bc9003c44f9b8d52bc9d96c
Lee Anderson <ecllips07@gmail.com>
2021-09-09 17:27:03 -0600

    added toppings page and buttons

--- client/src/components/Toppings.js
+++ client/src/components/Toppings.js
@@ -1,5 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import axios from "axios";
+import { Button, Card, Icon } from 'semantic-ui-react';
+import { Link } from "react-router-dom";
 
 const Toppings = (props) => {
   const [toppings, setToppings] = useState([]);
@@ -10,28 +12,47 @@ const Toppings = (props) => {
 
   const getToppings = async () => {
     try {
-      let res = await axios.get(`api/pizzas/${props.match.params.id}/toppings`);
+      let res = await axios.get(`/api/pizzas/${props.match.params.id}/toppings`);  //(`/api//pizza/toppings`);
       setToppings(res.data);
     } catch (err) {
       console.log(err);
     }
   };
 
+
+const deleteTopping = async (id) => {
+  try {
+    let res = await axios.delete(`/api/toppings/${id}`);
+    setToppings(toppings.filter((t) => t.id !== id));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
   const renderToppings = () => {
     return toppings.map((t) => {
       return (
-        <div>
-          <p>{t.category}</p>
-          <p>{t.name}</p>
-        </div>
+          <Card>
+            <Card.Content header={t.name} />
+            <Card.Content extra>{t.category}</Card.Content>
+            <Button.Group>
+              <Link to={`/toppings/${t.id}/edit`}>
+                <Button color="blue">Add</Button>
+              </Link>
+
+              <Button color="red" onClick={() => deleteTopping(t.id)}>
+                Remove
+              </Button>
+            </Button.Group>
+          </Card>
       );
     });
   };
+
+
   return (
     <div>
-      <h1>Toppings</h1>
-      <p>{renderToppings()}</p>
-     
+      <Card.Group>{renderToppings()}</Card.Group>
     </div>
   );
 };
